3bf39240737ed777e10c38ab1cbee75645a69560
Foudil <Foudil@Foudils-MacBook-Pro.local>
2016-12-04 15:04:10 +0100

    fix deloy step

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,3 +1,4 @@
+echo "Start deploying ..."
 host=$1
 ssh -o 'StrictHostKeyChecking no' $host
-scp -i deploy/foudil_key.pem target/package.zip ec2-user@$host:/tmp
+scp -i ../deploy/foudil_key.pem target/package.zip ec2-user@$host:/tmp
