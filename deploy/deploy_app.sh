9b141e97fb1a68eb66e70650bb649c0de4e11f15
Foudil <Foudil@Foudils-MacBook-Pro.local>
2016-12-04 15:44:50 +0100

    update deploy script

--- deploy/deploy_app.sh
+++ deploy/deploy_app.sh
@@ -2,4 +2,5 @@
 echo "Start deploying ..."
 host=52.30.96.161
 ssh -o 'StrictHostKeyChecking no' $host
+chmod 400 ../deploy/foudil_key.pem 
 scp -i ../deploy/foudil_key.pem target/package.zip ec2-user@$host:/tmp
